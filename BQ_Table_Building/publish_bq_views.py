97c119cf8625c17b8951b0d20716544007a012c2
wlongabaugh <wlongabaugh@systemsbiology.org>
2020-07-18 19:12:44 -0700

    debug

--- BQ_Table_Building/publish_bq_views.py
+++ BQ_Table_Building/publish_bq_views.py
@@ -261,7 +261,7 @@ def main(args):
                     sql_format = sql_format_file.read()
                 # use list as argument to format:
                 print(table_list)
-                view_sql = sql_format.format(*table_list)
+                view_sql = sql_format.format(table_list[0])
                 with open(metadata_file_full_path, mode='r') as view_metadata_file:
                     view_schema = json_loads(view_metadata_file.read())
                 success = create_view(target_client, target_project, dataset_id, view_name, view_schema, view_sql)
