24b47398ed2d561f00214fba69806c803340f213
Andrey Fedorov <andrey.fedorov@gmail.com>
2020-11-19 17:12:08 -0500

    further enhancements

--- BQ_metadata_checks/find_clashing_UIDs.sql
+++ BQ_metadata_checks/find_clashing_UIDs.sql
@@ -5,35 +5,62 @@ WITH
     WITH
       distinct_studies AS (
       SELECT
-        DISTINCT(StudyInstanceUID) AS UID
+        StudyInstanceUID AS UID,
+        "study" AS where_used,
+        STRING_AGG(DISTINCT(collection_id)) AS collections,
+        STRING_AGG(DISTINCT(PatientID)) AS patientIDs
       FROM
-        `idc-dev-etl.idc_tcia_views_dev.dicom_all`),
+        `idc-dev-etl.idc_tcia_views_dev.dicom_all`
+      GROUP BY
+        StudyInstanceUID),
       distinct_series AS (
       SELECT
-        DISTINCT(SeriesInstanceUID) AS UID
+        SeriesInstanceUID AS UID,
+        "series" AS where_used,
+        STRING_AGG(DISTINCT(collection_id)) AS collections,
+        STRING_AGG(DISTINCT(PatientID)) AS patientIDs
       FROM
-        `idc-dev-etl.idc_tcia_views_dev.dicom_all`),
+        `idc-dev-etl.idc_tcia_views_dev.dicom_all`
+      GROUP BY
+        SeriesInstanceUID),
       distinct_instances AS (
       SELECT
-        DISTINCT(SOPInstanceUID) AS UID
+        SOPInstanceUID AS UID,
+        "instance" AS where_used,
+        STRING_AGG(DISTINCT(collection_id)) AS collections,
+        STRING_AGG(DISTINCT(PatientID)) AS patientIDs
       FROM
-        `idc-dev-etl.idc_tcia_views_dev.dicom_all`)
+        `idc-dev-etl.idc_tcia_views_dev.dicom_all`
+      GROUP BY
+        SOPInstanceUID)
     SELECT
-      UID
+      UID,
+      where_used,
+      collections,
+      patientIDs
     FROM
       distinct_studies
     UNION ALL
     SELECT
-      UID
+      UID,
+      where_used,
+      collections,
+      patientIDs
     FROM
       distinct_series
     UNION ALL
     SELECT
-      UID
+      UID,
+      where_used,
+      collections,
+      patientIDs
     FROM
       distinct_instances)
   SELECT
     COUNT(UID) AS uid_counts,
+    STRING_AGG(DISTINCT(where_used)) AS where_used,
+    STRING_AGG(DISTINCT(collections)) AS collections,
+    STRING_AGG(DISTINCT(patientIDs)) AS patientIDs,
     uid
   FROM
     all_uids
@@ -42,9 +69,14 @@ WITH
   ORDER BY
     uid_counts DESC )
 SELECT
-  uid_counts,
-  uid
+  UID,
+  where_used,
+  collections,
+  patientIDs,
+  uid_counts
 FROM
   more_than_once_uids
 WHERE
   uid_counts > 1
+ORDER BY
+  collections
